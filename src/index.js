70cdf11c203b5fa8a087bfb3dc28fde2e070b6b9
Danilov Igor <danilovigor773@gmail.com>
2018-10-11 08:43:38 +0500

    Finished lesson 20 of js30 - speecj recognition

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import './index.scss';
 
-console.log("Hello World");
+window.SpeechRecognition = window.SpeechRecognition
+                          || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+
+words.appendChild(p);
+
+recognition.addEventListener('result', e => {
+  //console.log(e);
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+    //----puttiing the result into 'p' tag---
+    p.textContent = transcript;
+    //----Checking if th result is final, then creatin a new p tag--
+    if(e.results[0].isFinal){
+      p = document.createElement('p');
+      words.appendChild(p);
+    }
+  //console.log(e.result);
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
